d2a64ad608518b90b075d8bc6b0cae676e8fb9c6
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2018-04-11 16:57:28 -0300

    File handler class

--- src/app/LexicalAnalyzer.java
+++ src/app/LexicalAnalyzer.java
@@ -1,5 +1,18 @@
 package app;
 
+import dao.FileHandler;
+import java.util.List;
+import java.util.ArrayList;
+
 public class LexicalAnalyzer {
+
+    private List<String> sourceCode;
+
+    public LexicalAnalyzer(String fileName) {
+        this.sourceCode = FileHandler.getSourceCode(fileName);
+    }
+
     
+
+
 }
---
+++ src/dao/FileHandler.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FileHandler {
+    
+    public static List<String> getSourceCode(String fileName) {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String tmpLine;
+            List<String> sourceCode = new ArrayList<String>();
+            while((tmpLine = bufferedReader.readLine()) != null) {
+                sourceCode.add(tmpLine);
+            }
+            return sourceCode;
+        } catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe.toString());
+        } catch (IOException ioe) {
+            System.out.println(ioe.toString());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            return null;
+        }
+    }
+}
